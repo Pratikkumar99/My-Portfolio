8ab5ef5f39179f4682f9412f02500f0332a68b2d
Pratik Kumar <pratikk12093@gmail.com>
2026-09-08 18:59:25 +0530

    Cv changed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,8 +5,7 @@ import { getImageUrl } from "../../utils";
 export const Hero = () => {
   const roles = [
     "Full Stack Developer",
-    "PHP Developer",
-    "Next.Js Developer",
+    "Frontend Developer",
     "Backend Developer",
     "MERN Stack Developer",
   ];
