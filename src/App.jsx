334091dc726b987e9b423dcc1b03b921a5a9bf6b
Pratik Kumar <pratikk12093@gmail.com>
2026-09-24 13:49:57 +0530

    lenis : for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,24 @@ import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
   return (
     <div className={styles.App}>
       <Navbar />
